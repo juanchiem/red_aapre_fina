4fecd7cc99bec909746d521f0ae9707b3e93bf54
Juan Edwards Molina <edwardsmolina@gmail.com>
2023-03-27 09:00:11 -0300

    full analysis

---
+++ setup.R
@@ -0,0 +1,20 @@
+library(tidyverse) 
+library(ggpmisc)
+library(ggstance)
+library(huxtable)
+# library(patchwork)
+library(lme4)
+library(nlme)
+library(emmeans)
+library(multcomp)
+library(performance)
+
+conflicted::conflicts_prefer(ggplot2::annotate)
+conflicted::conflicts_prefer(dplyr::filter)
+conflicted::conflict_prefer("select", "dplyr")
+
+theme_set(theme_bw(base_size = 12)) 
+
+knitr::opts_chunk$set(echo = F, warning = FALSE, message = FALSE)
+
+asin_tran <- make.tran("asin.sqrt", 100)
